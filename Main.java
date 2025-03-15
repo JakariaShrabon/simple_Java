818a0e9394c461f021fca801f6d81ddb1667d4ec
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2025-03-16 01:51:40 +0600

    Add files via upload
    First comment

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //different between println vs print
+        //if we use println
+        System.out.println("Hello");
+        System.out.println(" World!");
+        //we gonna see println print in new line
+        //if we use print
+        System.out.print("Hello");
+        System.out.print(" World!");
+        //we gonna see it is print in same line
+
+    }
+}
---
+++ loops.java
@@ -0,0 +1,16 @@
+public class loops {
+    public static void main(String[] args) {
+        // Normal loop
+        for(int i = 0; i < 10; i++)
+            System.out.println(i);
+
+        //for each loop is kind of loop
+
+        String[] names = {"Ali", "Shrabon", "Jalil"};
+
+        for (String name : names){
+            System.out.println(name);
+        }
+    }
+
+}
