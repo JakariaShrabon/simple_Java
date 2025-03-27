8b18c3adca0cb6b146cd66f198efaea67af44be7
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2025-03-27 22:19:11 +0600

    Add files via upload

---
+++ Ex_Arraylist.java
@@ -0,0 +1,14 @@
+package Java_collections;
+
+import java.util.ArrayList;
+
+public class Ex_Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
