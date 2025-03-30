6c45262308c1ae878e6f0251a35778e30dcbf4d4
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2025-03-31 04:30:54 +0600

    Add files via upload

---
+++ example_of_set.js
@@ -0,0 +1,15 @@
+// we use collection of data with out duplicates
+
+//.... some simple example of it...
+
+const mySet = new Set();
+
+
+mySet.add(1);
+mySet.add(2);
+mySet.add(3);
+mySet.add(2);
+
+console.log(mySet);
+
+
